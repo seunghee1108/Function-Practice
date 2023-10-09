8bd7ff8ad862f5e63de9c7f2ed1c3f928da197bd
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-09 19:27:17 +0900

    minus 함수

---
+++ minus.js
@@ -0,0 +1,8 @@
+function minus(firstNumber, secondNumber) {
+  if(typeof(firstNumber) === "number" && typeof(secondNumber) === "number") {
+    return firstNumber - secondNumber;
+  } else {
+    console.log(firstNumber, "타입이 잘못 지정되었습니다.");
+  }
+}
+module.exports = minus;
